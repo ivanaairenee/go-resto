ce90b7aa7c9a1e27b0e1853493d86195bede2426
IVANA IRENE THOMAS <ivana.irene@ui.ac.id>
2020-03-14 21:18:50 +0700

    initial commit

---
+++ spec/views/menus/index.html.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "menus/index", type: :view do
+  before(:each) do
+    assign(:menus, [
+      Menu.create!(
+        :name => "Name",
+        :description => "Description",
+        :price => 2
+      ),
+      Menu.create!(
+        :name => "Name",
+        :description => "Description",
+        :price => 2
+      )
+    ])
+  end
+
+  it "renders a list of menus" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Description".to_s, :count => 2
+    assert_select "tr>td", :text => 2.to_s, :count => 2
+  end
+end
---
+++ spec/views/menus/show.html.erb_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "menus/show", type: :view do
+  before(:each) do
+    @menu = assign(:menu, Menu.create!(
+      :name => "Name",
+      :description => "Description",
+      :price => 2
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Description/)
+    expect(rendered).to match(/2/)
+  end
+end
