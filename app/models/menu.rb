c42bbe17580630b9baa03d458639f7a2a1e4292b
Ahmad Izzan <aahmadizzan@gmail.com>
2020-03-14 22:12:47 +0700

    Add spec for Menu model.

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,2 +1,5 @@
 class Menu < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
 end
--- spec/models/menu_spec.rb
+++ spec/models/menu_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Menu, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject {
+    described_class.new(name: 'Latte',
+                        description: 'Cold',
+                        price: 35000)
+  }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a description' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid with string typed price' do
+    subject.price = 'not a number'
+    expect(subject).to_not be_valid
+  end
 end
